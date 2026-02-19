85b6ff86748aac1ed5aea5258eede9b4e7554071
Ariel Mosquera Ribeiro <arielmosquera015@outlook.com>
2026-02-19 13:42:48 +0100

    Avanzando en los ejercicios

--- src/main/java/es/uvigo/esei/aed1/activity3/doublyLinkedDummy/DoublyLinkedDummy.java
+++ src/main/java/es/uvigo/esei/aed1/activity3/doublyLinkedDummy/DoublyLinkedDummy.java
@@ -5,37 +5,56 @@ import es.uvigo.esei.aed1.commonLinked.DoubleNode;
 
 public class DoublyLinkedDummy {
 
+  private DoubleNode first;
+  private DoubleNode last;
+  private int numOfValues;
 
   public DoublyLinkedDummy() {
-    
+    this.first = new DoubleNode(null, 0, null);
+    this.last = new DoubleNode(this.first, 0, null);
+    this.first.setNext(this.last);
+    this.numOfValues = 0;
   }
 
   public boolean isEmpty() {
-    return true;
+    return this.numOfValues == 0;
   }
 
   public int size() {
-    return 0;
+    return this.numOfValues;
   }
 
   public int numberOfOccurrences(int value) {
-    return 0;
+    var counter = 0;
+
+    for (DoubleNode current = this.first.getNext(); current != null; current = current.getNext()) {
+      if (current.hasValue(value))
+        counter++;
+    }
+
+    return counter;
   }
 
   public boolean contains(int value) {
-    return true;
+
+    DoubleNode current = this.first.getNext();
+
+    while (current != null && !current.hasValue(value))
+      current = current.getNext();
+
+    return current != null;
   }
 
   public void addFirst(int value) {
-    
+
   }
 
   public void addLast(int value) {
-    
+
   }
 
   public void remove(int value) {
-    
+
   }
 
   @Override
