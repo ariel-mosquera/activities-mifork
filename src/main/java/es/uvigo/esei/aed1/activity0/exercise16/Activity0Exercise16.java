20fffe53b72bb0e3f893a22ef1cb8cfe21a84972
Ariel Mosquera Ribeiro <arielmosquera015@outlook.com>
2026-02-21 15:53:03 +0100

    Haciendo algunos ejercicios basicos

--- src/main/java/es/uvigo/esei/aed1/activity0/exercise16/Activity0Exercise16.java
+++ src/main/java/es/uvigo/esei/aed1/activity0/exercise16/Activity0Exercise16.java
@@ -1,50 +1,84 @@
 
 package es.uvigo.esei.aed1.activity0.exercise16;
+
 import java.util.Scanner;
 
 public class Activity0Exercise16 {
 
-  //exercise 16
-  
+  // exercise 16
+
   /**
    * Calcula el maximo de los enteros almacenados en el array
+   * 
    * @param array contiene los enteros
    * @return el valor mínimo
    */
   public static int maximum(int[] array) {
-    
-    return 0;
+    var max = array[0];
+
+    for (int i = 0; i < array.length; i++) {
+      if (max < array[i])
+        max = array[i];
+    }
+
+    return max;
   }
 
   /**
    * Calcula el mínimo de los enteros almacenados en el array
+   * 
    * @param array contiene los enteros
    * @return el valor mínimo
    */
   public static int minimum(int[] array) {
-    
-    return 0;
+    var min = array[0];
+
+    for (int i = 0; i < array.length; i++) {
+      if (array[i] < min)
+        min = array[i];
+    }
+
+    return min;
   }
-/**
- * Calcula el promedio de los enteros almacenados en el array
- * @param array contiene los enteros
- * @return el valor promedio
- */
-  public static double average(int[] array) { 
-    
-    return 0;
+
+  /**
+   * Calcula el promedio de los enteros almacenados en el array
+   * 
+   * @param array contiene los enteros
+   * @return el valor promedio
+   */
+  public static double average(int[] array) {
+    var suma = 0;
+
+    for (int i = 0; i < array.length; i++) {
+      suma += array[i];
+    }
+
+    return (double) suma / array.length;
   }
 
   public static int readInteger(String message) {
-    
-    return 0;
+    var teclado = new Scanner(System.in);
+
+    System.out.print("\n" + message);
+    int num = Integer.parseInt(teclado.nextLine());
+    teclado.close();
+
+    return num;
   }
+
   /**
    * almacena enteros en el array haciendo uso del método readInteger
+   * 
    * @param array que almacenará los enteros
    */
   public static void readArray(int[] array) {
-    
+
+    for (int i = 0; i < array.length; i++) {
+      System.out.println("Indice [" + i + "]");
+      array[i] = readInteger("Introduce un entero: ");
+    }
+
   }
-  
+
 }
