d80f27a88b0ce86b8715105b0b8672da1191acd3
Ariel Mosquera Ribeiro <arielmosquera015@outlook.com>
2026-02-22 15:32:16 +0100

    Metodos del ejercicio 17

--- src/main/java/es/uvigo/esei/aed1/activity0/exercise17/Activity0Exercise17.java
+++ src/main/java/es/uvigo/esei/aed1/activity0/exercise17/Activity0Exercise17.java
@@ -13,7 +13,13 @@ public class Activity0Exercise17 {
    * @return el número entero leido de teclado
    */
   public static int readNumber(String message) {
-    return 0;
+    Scanner teclado = new Scanner(System.in);
+
+    System.out.print("\n" + message);
+    var num = Integer.parseInt(teclado.nextLine());
+    teclado.close();
+
+    return num;
   }
 
   /**
@@ -23,7 +29,13 @@ public class Activity0Exercise17 {
    * @return el número real leido de teclado
    */
   public static double readDouble(String message) {
-    return 0;
+    Scanner teclado = new Scanner(System.in);
+
+    System.out.print("\n" + message);
+    var num = Double.parseDouble(teclado.nextLine());
+    teclado.close();
+
+    return num;
   }
 
   /**
@@ -33,7 +45,13 @@ public class Activity0Exercise17 {
    * @param message el mensaje a visualizar
    */
   public static void readNotes(double[][] matrix, String message) {
-
+    System.out.println(message);
+    for (int i = 0; i < matrix.length; i++) { // ALumnos
+      System.out.println("Aluno [" + (i + 1) + "]:");
+      for (int j = 0; j < matrix[i].length; j++) { // Asignaturas
+        matrix[i][j] = readDouble("Nota de la asignatura [" + (j + 1) + "]: ");
+      }
+    }
   }
 
   /**
@@ -44,6 +62,13 @@ public class Activity0Exercise17 {
    * @param message el mensaje a visualizar
    */
   public static void printNotes(double[][] matrix, String message) {
+    System.out.println(message);
 
+    for (int i = 0; i < matrix.length; i++) {
+      System.out.println("Alumno [" + (i + 1) + "]: ");
+      for (int j = 0; j < matrix[i].length; j++) {
+        System.out.println("Nota [" + (j + 1) + "]: " + matrix[i][j] + "\t");
+      }
+    }
   }
 }
