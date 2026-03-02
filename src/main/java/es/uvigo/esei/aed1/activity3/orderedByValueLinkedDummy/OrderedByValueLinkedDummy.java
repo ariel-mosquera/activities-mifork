adc135dad676cbc5f8ca8daf8962d24bf9ac4efc
Ariel Mosquera Ribeiro <arielmosquera015@outlook.com>
2026-03-02 12:09:10 +0100

    Implementation of the class orderedByValue with dummy node

--- src/main/java/es/uvigo/esei/aed1/activity3/orderedByValueLinkedDummy/OrderedByValueLinkedDummy.java
+++ src/main/java/es/uvigo/esei/aed1/activity3/orderedByValueLinkedDummy/OrderedByValueLinkedDummy.java
@@ -2,25 +2,80 @@ package es.uvigo.esei.aed1.activity3.orderedByValueLinkedDummy;
 
 public class OrderedByValueLinkedDummy {
 
-  
+  private NodePair dummy;
+  private int numOfValues;
+
   public OrderedByValueLinkedDummy() {
-    
+    this.dummy = new NodePair(null, null);
+    this.numOfValues = 0;
   }
 
   public boolean contains(int value) {
-    return true;
+
+    NodePair current = dummy.getNext();
+
+    while (current != null && current.getPair().getValue() < value)
+      current = current.getNext();
+
+    return !(current == null || current.getPair().getValue() < value);
   }
 
   public void add(int value) {
-    
+    NodePair current = dummy;
+
+    while (current.getNext() != null && current.getNext().getPair().getValue() < value)
+      current = current.getNext();
+
+    /*
+     * if (current.getNext().getPair().getValue() == value)
+     * current.getNext().getPair().setCounter(current.getPair().getCounter() + 1);
+     * else { NodePair newNode = new NodePair(new Pair(1, value),
+     * current.getNext()); current.setNext(newNode); }
+     */
+
+    if (current.getNext() == null || current.getNext().getPair().getValue() > value) {
+      NodePair newNode = new NodePair(new Pair(1, value), current.getNext());
+      current.setNext(newNode);
+    }
+    else {
+      Pair myPair = current.getNext().getPair();
+      myPair.setCounter(myPair.getCounter() + 1);
+    }
+
+    numOfValues++;
   }
 
   public void remove(int value) {
-    
+    if (dummy.getNext() == null)
+      System.out.println("Empty structure");
+    else {
+      NodePair current = dummy;
+
+      while (current.getNext() != null && current.getNext().getPair().getValue() < value)
+        current = current.getNext();
+
+      if (current.getNext() == null || current.getNext().getPair().getValue() > value)
+        System.out.println("The value is not in the structure...");
+      else {
+        Pair myPair = current.getNext().getPair();
+
+        if (myPair.getCounter() == 1)
+          current.setNext(current.getNext().getNext());
+        else
+          current.getNext().getPair().setCounter(myPair.getCounter() - 1);
+      }
+      numOfValues--;
+    }
   }
 
   @Override
   public String toString() {
-    return "";
+    StringBuilder cad = new StringBuilder();
+
+    for (NodePair current = dummy.getNext(); current != null; current = current.getNext()) {
+      cad.append(current.getPair()).append(" ");
+    }
+
+    return cad.toString();
   }
 }
--- src/main/java/es/uvigo/esei/aed1/activity3/orderedByValueLinkedDummy/Pair.java
+++ src/main/java/es/uvigo/esei/aed1/activity3/orderedByValueLinkedDummy/Pair.java
@@ -1,34 +1,34 @@
 
 package es.uvigo.esei.aed1.activity3.orderedByValueLinkedDummy;
 
-
 public class Pair {
 
   private int counter;
   private int value;
 
   public Pair(int counter, int value) {
-    
+    this.counter = counter;
+    this.value = value;
   }
 
   public int getCounter() {
-    return 0;
+    return this.counter;
   }
 
   public int getValue() {
-    return 0;
+    return this.value;
   }
 
   public void setCounter(int counter) {
-    
+    this.counter = counter;
   }
 
   public void setValue(int value) {
-    
+    this.value = value;
   }
 
   @Override
   public String toString() {
-    return "";
+    return "[" + counter + ", " + value + "]";
   }
 }
