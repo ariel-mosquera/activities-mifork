00a1ef50c0c47555e75969b66002949e3904d2bd
Ariel Mosquera Ribeiro <arielmosquera015@outlook.com>
2026-03-09 00:05:09 +0100

    Implementando clase de gestión de estructura doblemente enlazada con centinelas al principio y al final

--- src/main/java/es/uvigo/esei/aed1/activity3/doublyLinkedDummy/DoublyLinkedDummy.java
+++ src/main/java/es/uvigo/esei/aed1/activity3/doublyLinkedDummy/DoublyLinkedDummy.java
@@ -5,15 +5,14 @@ import es.uvigo.esei.aed1.commonLinked.DoubleNode;
 
 public class DoublyLinkedDummy {
 
-  private DoubleNode first;
-  private DoubleNode last;
+  private DoubleNode firstDummy;
+  private DoubleNode lastDummy;
   private int numOfValues;
 
   public DoublyLinkedDummy() {
-    this.first = new DoubleNode(null, 0, null);
-    this.last = new DoubleNode(this.first, 0, null);
-    this.first.setNext(this.last);
-    this.numOfValues = 0;
+    firstDummy = new DoubleNode(null, 0, null);
+    lastDummy = new DoubleNode(firstDummy, 0, null);
+    firstDummy.setNext(lastDummy);
   }
 
   public boolean isEmpty() {
@@ -27,7 +26,7 @@ public class DoublyLinkedDummy {
   public int numberOfOccurrences(int value) {
     var counter = 0;
 
-    for (DoubleNode current = this.first.getNext(); current != null; current = current.getNext()) {
+    for (DoubleNode current = this.firstDummy.getNext(); current != null; current = current.getNext()) {
       if (current.hasValue(value))
         counter++;
     }
@@ -37,29 +36,58 @@ public class DoublyLinkedDummy {
 
   public boolean contains(int value) {
 
-    DoubleNode current = this.first.getNext();
+    DoubleNode current = this.firstDummy.getNext();
 
-    while (current != null && !current.hasValue(value))
+    while (current != lastDummy && !current.hasValue(value))
       current = current.getNext();
 
-    return current != null;
+    return current != lastDummy;
   }
 
   public void addFirst(int value) {
+    DoubleNode newNode = new DoubleNode(firstDummy, value, firstDummy.getNext());
 
+    firstDummy.getNext().setPrevious(newNode);
+    firstDummy.setNext(newNode);
+    numOfValues++;
   }
 
   public void addLast(int value) {
+    DoubleNode newNode = new DoubleNode(lastDummy.getPrevious(), value, lastDummy);
 
+    lastDummy.getPrevious().setNext(newNode);
+    lastDummy.setPrevious(newNode);
+    numOfValues++;
   }
 
   public void remove(int value) {
+    if (isEmpty())
+      System.out.println("Empty structure");
+    else {
+      DoubleNode current = firstDummy.getNext();
 
+      while (current != lastDummy && !current.hasValue(value))
+        current = current.getNext();
+
+      if (current != lastDummy) {
+        current.getNext().setPrevious(current.getPrevious());
+        current.getPrevious().setNext(current.getNext());
+        numOfValues--;
+      }
+      else
+        System.out.println("The value is not in the structure to be removed...");
+    }
   }
 
   @Override
   public String toString() {
-    return "";
+    StringBuilder cad = new StringBuilder();
+
+    for (DoubleNode current = firstDummy.getNext(); current != lastDummy; current = current.getNext()) {
+      cad.append(current.getValue()).append(' ');
+    }
+
+    return cad.toString();
   }
 
 }
