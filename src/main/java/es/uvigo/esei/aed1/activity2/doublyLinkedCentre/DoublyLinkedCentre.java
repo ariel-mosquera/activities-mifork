45f5fdd712ff48038e3709194cb500c3b926da34
Ariel Mosquera Ribeiro <arielmosquera015@outlook.com>
2026-03-08 23:50:20 +0100

    Implementando clase de gestion de una estructura circular doblemente enlazada con centinela

--- src/main/java/es/uvigo/esei/aed1/activity2/doublyLinkedCentre/DoublyLinkedCentre.java
+++ src/main/java/es/uvigo/esei/aed1/activity2/doublyLinkedCentre/DoublyLinkedCentre.java
@@ -3,37 +3,35 @@ package es.uvigo.esei.aed1.activity2.doublyLinkedCentre;
 
 import es.uvigo.esei.aed1.commonLinked.DoubleNode;
 
-
 public class DoublyLinkedCentre {
 
- 
-  
-  public DoublyLinkedCentre() { 
-    
+  private DoubleNode centre;
+  private int numOfValues;
+
+  public DoublyLinkedCentre() {
+
   }
 
   public boolean isEmpty() {
-    
-    return true;
+    return numOfValues == 0;
   }
 
   public int size() {
-    
-    return 0;
+    return numOfValues;
   }
 
   public boolean contains(int value) {
-    
+
     return true;
   }
 
   public void add(int value) {
-    
+
   }
 
   @Override
   public String toString() {
-    
+
     return "";
   }
 
--- src/main/java/es/uvigo/esei/aed1/activity3/circularDoublyLinkedDummy/CircularDoublyLinkedDummy.java
+++ src/main/java/es/uvigo/esei/aed1/activity3/circularDoublyLinkedDummy/CircularDoublyLinkedDummy.java
@@ -5,42 +5,88 @@ import es.uvigo.esei.aed1.commonLinked.DoubleNode;
 
 public class CircularDoublyLinkedDummy {
 
-  public CircularDoublyLinkedDummy() {
+  private DoubleNode lastDummy;
+  private int numOfValues;
 
+  public CircularDoublyLinkedDummy() {
+    lastDummy = new DoubleNode(null, 0, null);
+    lastDummy.setNext(lastDummy);
+    lastDummy.setPrevious(lastDummy);
+    numOfValues = 0;
   }
 
   public boolean isEmpty() {
-    return false;
+    return numOfValues == 0;
   }
 
   public int size() {
-    return 0;
+    return numOfValues;
   }
 
   public int numberOfOccurrences(int value) {
-    return 0;
+    int counter = 0;
+
+    for (DoubleNode current = lastDummy.getNext(); current != lastDummy; current = current.getNext()) {
+      if (current.hasValue(value))
+        counter++;
+    }
+
+    return counter;
   }
 
   public boolean contains(int value) {
-    return true;
+    DoubleNode current = lastDummy.getNext();
+
+    while (current != lastDummy && !current.hasValue(value))
+      current = current.getNext();
+
+    return current != lastDummy;
   }
 
   public void addFirst(int value) {
+    DoubleNode newNode = new DoubleNode(lastDummy, value, lastDummy.getNext());
 
+    lastDummy.getNext().setPrevious(newNode);
+    lastDummy.setNext(newNode);
+    numOfValues++;
   }
 
   public void addLast(int value) {
+    DoubleNode newNode = new DoubleNode(lastDummy.getPrevious(), value, lastDummy);
 
+    lastDummy.getPrevious().setNext(newNode);
+    lastDummy.setPrevious(newNode);
+    numOfValues++;
   }
 
   public void remove(int value) {
+    if (isEmpty())
+      System.out.println("Empty structure, there is no values to remove");
+    else {
+      DoubleNode current = lastDummy.getNext();
 
+      while (current != lastDummy && !current.hasValue(value))
+        current = current.getNext();
+
+      if (current != lastDummy) {
+        current.getNext().setPrevious(current.getPrevious());
+        current.getPrevious().setNext(current.getNext());
+        numOfValues--;
+      }
+      else
+        System.out.println("The value is not in the structure");
+    }
   }
 
   @Override
   public String toString() {
+    StringBuilder cad = new StringBuilder();
 
-    return "";
+    for (DoubleNode current = lastDummy.getNext(); current != lastDummy; current = current.getNext()) {
+      cad.append(current.getValue()).append(' ');
+    }
+
+    return cad.toString();
   }
 
 }
