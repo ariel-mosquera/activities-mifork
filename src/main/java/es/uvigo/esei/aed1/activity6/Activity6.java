323f732de892777884a815e0fa2d677536583146
Ariel Mosquera Ribeiro <arielmosquera015@outlook.com>
2026-03-23 16:25:24 +0100

    Refactor and implement equalsValues logic in activity

--- src/main/java/es/uvigo/esei/aed1/activity6/Activity6.java
+++ src/main/java/es/uvigo/esei/aed1/activity6/Activity6.java
@@ -1,45 +1,59 @@
 
 package es.uvigo.esei.aed1.activity6;
 
+import static java.util.Objects.requireNonNull;
+
 import es.uvigo.esei.aed1.activity6.implementation.CustomQueue;
 import es.uvigo.esei.aed1.tads.queue.Queue;
 
-
 public class Activity6 {
-    
-    //Exercise 1.1
+
+    // Exercise 1.1
     public static <T> void concat(Queue<T> queue1, Queue<T> queue2) throws NullPointerException {
-        
+
     }
 
-    //Exercise 1.2
-    public static <T> Queue<T> mix(Queue<T> queue1, Queue<T> queue2) throws NullPointerException{
+    // Exercise 1.2
+    public static <T> Queue<T> mix(Queue<T> queue1, Queue<T> queue2) throws NullPointerException {
         return null;
     }
 
-    //Exercise 2
-    public static <T> Queue<T> copy(Queue<T> queue) throws NullPointerException{
+    // Exercise 2
+    public static <T> Queue<T> copy(Queue<T> queue) throws NullPointerException {
         return null;
     }
 
-    //Exercise 3
-    public static Queue<Integer> mixInOrderly(Queue<Integer> queue1, Queue<Integer> queue2) throws NullPointerException {
+    // Exercise 3
+    public static Queue<Integer> mixInOrderly(Queue<Integer> queue1, Queue<Integer> queue2)
+            throws NullPointerException {
         return null;
     }
 
-    //Exercise 4
-    public static <T> void moveToFront(Queue<T> queue, T value) throws NullPointerException{
-        
+    // Exercise 4
+    public static <T> void moveToFront(Queue<T> queue, T value) throws NullPointerException {
+
     }
 
-    //Exercise 5
+    // Exercise 5
     public static Integer josephus(Queue<Integer> soldiers, int initialPos, int jump) {
         return 0;
     }
-    
-    //Ejercicio 6
+
+    // Ejercicio 6
     public static <T> boolean equalsValues(CustomQueue<T> queue) throws NullPointerException {
-       return true; 
+        requireNonNull(queue, "The queue can't be null");
+
+        T firstValue = queue.first();
+        boolean valid = true;
+        int size = queue.size();
+
+        for (int i = 0; i < size; i++) {
+            if (!queue.first().equals(firstValue))
+                valid = false;
+            queue.add(queue.remove());
+        }
+
+        return valid;
     }
 
 }
