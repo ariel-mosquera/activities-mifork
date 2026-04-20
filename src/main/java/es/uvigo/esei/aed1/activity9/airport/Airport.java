0b1604cd5d957588871b4c833bcf09c3031d5cf7
Ariel Mosquera Ribeiro <arielmosquera015@outlook.com>
2026-04-20 12:30:07 +0200

    Implement Airport runway management logic

--- src/main/java/es/uvigo/esei/aed1/activity9/airport/Airport.java
+++ src/main/java/es/uvigo/esei/aed1/activity9/airport/Airport.java
@@ -5,27 +5,53 @@ public class Airport {
     private Runway[] runways;
 
     public Airport(int numRunways) {
+        runways = new Runway[numRunways];
+
+        for (int i = 0; i < numRunways; i++)
+            runways[i] = new Runway(i);
 
     }
 
     public void assignDestinationRunway(int numRunway, String destination) {
-
+        runways[numRunway].assignDestination(destination);
     }
 
     public void assignFlightRunway(Flight v) {
+        /*
+         * Runway targetRunway = null;
+         * 
+         * for (Runway runway : runways) { if (runway.isDestination(v.getDestination())) { if (targetRunway == null || runway.numberFlight() < targetRunway.numberFlight()) targetRunway = runway; } }
+         * 
+         * targetRunway.assignFlight(v);
+         */
 
+        int numFlights = Integer.MAX_VALUE;
+        int numRunway = -1;
+
+        for (int i = 0; i < runways.length; i++) {
+            if (runways[i].isDestination(v.getDestination()) && runways[i].numberFlight() < numFlights) {
+                numFlights = runways[i].numberFlight();
+                numRunway = i;
+            }
+        }
+
+        runways[numRunway].assignFlight(v);
     }
 
     public Flight takeoffFlight(int numRunway) {
-        return null;
+        return runways[numRunway].removeFlight();
     }
 
     public int getNumRunways() {
-        return 0;
+        return runways.length;
     }
 
     public int getNumAssignedFlights() {
+        int numAssignedFlights = 0;
 
-        return -1;
+        for (Runway runway : runways)
+            numAssignedFlights += runway.numberFlight();
+
+        return numAssignedFlights;
     }
 }
