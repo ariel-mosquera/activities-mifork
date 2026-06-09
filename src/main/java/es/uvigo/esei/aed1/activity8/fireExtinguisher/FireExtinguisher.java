e152b45b4adf0b560272e106949da0440e9f9e98
Ariel Mosquera Ribeiro <arielmosquera015@outlook.com>
2026-06-09 18:38:46 +0200

    Fix indentation in FireExtinguisher class

--- src/main/java/es/uvigo/esei/aed1/activity8/fireExtinguisher/FireExtinguisher.java
+++ src/main/java/es/uvigo/esei/aed1/activity8/fireExtinguisher/FireExtinguisher.java
@@ -2,10 +2,10 @@
 package es.uvigo.esei.aed1.activity8.fireExtinguisher;
 
 public class FireExtinguisher {
-private final int referenceNumber;
-private final char type;
-private final double price;
-private final String dateStamped;  
+    private final int referenceNumber;
+    private final char type;
+    private final double price;
+    private final String dateStamped;
 
     public FireExtinguisher(int referenceNumber, char type, double price, String dateStamped) {
         this.referenceNumber = referenceNumber;
