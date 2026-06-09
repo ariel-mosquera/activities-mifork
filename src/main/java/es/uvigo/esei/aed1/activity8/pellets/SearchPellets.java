4fe9f9f2f5b4b1f5fa9c6db8e8e45e974a1e7f16
Ariel Mosquera Ribeiro <arielmosquera015@outlook.com>
2026-06-09 18:38:46 +0200

    Implement pellet search in 2D matrix

--- src/main/java/es/uvigo/esei/aed1/activity8/pellets/SearchPellets.java
+++ src/main/java/es/uvigo/esei/aed1/activity8/pellets/SearchPellets.java
@@ -2,9 +2,17 @@ package es.uvigo.esei.aed1.activity8.pellets;
 
 public class SearchPellets {
 
-    //Exercise 7
-    public static int searchPellets(int[][] matrizCoastGalician, Position beginning, Position fin) {
+    // Exercise 7
+    public static int searchPellets(int[][] galicianCoastMatrix, Position beginning, Position end) {
+        int counter = 0;
 
-        return -1;
+        for (int i = beginning.getX(); i <= end.getX(); i++) {
+            for (int j = beginning.getY(); j <= end.getY(); j++) {
+                if (galicianCoastMatrix[i][j] == 0)
+                    counter++;
+            }
+        }
+
+        return counter;
     }
 }
