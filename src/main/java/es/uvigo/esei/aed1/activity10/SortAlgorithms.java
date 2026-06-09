48cb4c6bfd922093c50ddd55318814ce75130f3c
Ariel Mosquera Ribeiro <arielmosquera015@outlook.com>
2026-06-09 18:38:46 +0200

    Add generic and advanced sorting implementations

--- src/main/java/es/uvigo/esei/aed1/activity10/SortAlgorithms.java
+++ src/main/java/es/uvigo/esei/aed1/activity10/SortAlgorithms.java
@@ -112,10 +112,91 @@ public class SortAlgorithms {
             }
         } while (right <= left);
 
-        return right; //primera posición de la segunda mitad
+        return right; // primera posición de la segunda mitad
     }
 
     public static void quickSort(int[] aux, int beginning, int fin) {
 
+    }
+
+    public static <T extends Comparable<? super T>> void insertionSort(T[] aux, int numOfElem) {
+        for (int i = 1; i < numOfElem; i++) {
+            int j = i - 1;
+            T current = aux[i];
+
+            while (j >= 0 && current.compareTo(aux[j]) < 0)
+                aux[j + 1] = aux[j--];
+
+            aux[j + 1] = current;
+        }
+    }
+
+    public static <T extends Comparable<? super T>> void selectionSort(T[] aux, int numOfElem) {
+        for (int i = 0; i < numOfElem; i++) {
+            int smallerPos = i;
+
+            for (int j = i + 1; j < numOfElem; j++) {
+                if (aux[j].compareTo(aux[smallerPos]) < 0)
+                    smallerPos = j;
+            }
+
+            if (smallerPos != i) {
+                T temp = aux[i];
+                aux[i] = aux[smallerPos];
+                aux[smallerPos] = temp;
+            }
+        }
+    }
+
+    public static <T extends Comparable<? super T>> void bubbleSort(T[] aux, int numOfElem) {
+        boolean isSwap = true;
+        int counter = 1;
+
+        while (isSwap && counter < aux.length) {
+            isSwap = false;
+            for (int i = 1; i <= numOfElem - counter; i++) {
+                if (aux[i - 1].compareTo(aux[i]) > 0) {
+                    T temp = aux[i];
+                    aux[i] = aux[i - 1];
+                    aux[i - 1] = temp;
+                    isSwap = true;
+                }
+            }
+            counter++;
+        }
+    }
+
+    public static void mergeSort(int[] array, int start, int end) {
+        if (start < end) {
+            int middle = (start + end) / 2;
+            mergeSort(array, start, middle);
+            mergeSort(array, middle + 1, end);
+
+            merge(array, start, middle, middle + 1, end);
+        }
+    }
+
+    private static void merge(int[] array, int leftStart, int leftEnd, int rightStart, int rightEnd) {
+        LinkedList<Integer> aux = new LinkedList<>();
+
+        int i = leftStart;
+        int j = rightStart;
+
+        while (i <= leftEnd && j <= rightEnd) {
+            if (array[i] < array[j])
+                aux.addFirst(array[i++]);
+            else
+                aux.addFirst(array[j++]);
+        }
+
+        while (i <= leftEnd)
+            aux.addFirst(array[i++]);
+
+        while (j <= rightEnd)
+            aux.addLast(array[j++]);
+
+        for (int k = leftStart; k < rightEnd; k++) {
+            array[k] = aux.removeFirst();
+        }
     }
 }
